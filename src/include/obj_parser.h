898ff51bb23499e1009e1510be5d8018d0a03b72
Kirill Agafonov <kaagafonov@edu.hse.ru>
2025-01-29 13:41:50 +0300

    Some minor changes

--- src/include/obj_parser.h
+++ src/include/obj_parser.h
@@ -7,11 +7,14 @@
 
 class ObjParser {
 public:
-    static Object LoadObj(const std::string& filename, const Eigen::Vector3d& translation,
-                          const Eigen::Matrix3d& rotation, uint32_t color = 0xFF0000);
+    static Object LoadObj(const std::string& filename,
+                          const Eigen::Vector3d& translation = Eigen::Vector3d::Zero(),
+                          const Eigen::Matrix3d& rotation = Eigen::Matrix3d::Identity(),
+                          uint32_t color = 0xFFFFFF);
 
 private:
     static std::vector<Eigen::Vector3d> ParseVertices(const std::string& content);
+
     static std::vector<Triangle> ParseFaces(const std::string& content,
                                             const std::vector<Eigen::Vector3d>& vertices,
                                             uint32_t color);

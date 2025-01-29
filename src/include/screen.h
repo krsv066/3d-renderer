4df7cca527e9016df0012f0e5c6a13ff318d2915
Kirill Agafonov <kaagafonov@edu.hse.ru>
2025-01-29 19:16:18 +0300

    Add screen class

--- src/include/screen.h
+++ src/include/screen.h
@@ -1,2 +1,12 @@
 #pragma once
 
+#include <cstdint>
+
+class Screen {
+public:
+    Screen(uint32_t width, uint32_t height);
+
+private:
+    uint32_t width_;
+    uint32_t height_;
+};
---
+++ src/screen/screen.cpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "screen.h"
+
+Screen::Screen(uint32_t width, uint32_t height) : width_(width), height_(height) {
+}

723c31f6eaf5482bee98d732624c2921a7d8f961
Kirill Agafonov <kaagafonov@edu.hse.ru>
2025-03-29 17:32:34 +0300

    Fix Color class

--- src/utils/color.cpp
+++ src/utils/color.cpp
@@ -3,57 +3,60 @@
 
 namespace renderer {
 static constexpr double kMaxHexColorComponent = 255.0;
+static constexpr int kRedShift = 16;
+static constexpr int kGreenShift = 8;
+static constexpr uint32_t kComponentMask = 0xff;
 
 Color::Color(uint32_t hex)
-    : r_(Red(((hex >> 16) & 0xFF) / kMaxHexColorComponent)),
-      g_(Green(((hex >> 8) & 0xFF) / kMaxHexColorComponent)),
-      b_(Blue((hex & 0xFF) / kMaxHexColorComponent)) {
+    : r_(((hex >> kRedShift) & kComponentMask) / kMaxHexColorComponent),
+      g_(((hex >> kGreenShift) & kComponentMask) / kMaxHexColorComponent),
+      b_((hex & kComponentMask) / kMaxHexColorComponent) {
+}
+
+Color::Color(double r, double g, double b) : r_(r), g_(g), b_(b) {
 }
 
 uint32_t Color::GetHex() const {
-    uint8_t r_component = static_cast<uint8_t>(r_.value * kMaxHexColorComponent);
-    uint8_t g_component = static_cast<uint8_t>(g_.value * kMaxHexColorComponent);
-    uint8_t b_component = static_cast<uint8_t>(b_.value * kMaxHexColorComponent);
+    uint8_t r_component = static_cast<uint8_t>(r_ * kMaxHexColorComponent);
+    uint8_t g_component = static_cast<uint8_t>(g_ * kMaxHexColorComponent);
+    uint8_t b_component = static_cast<uint8_t>(b_ * kMaxHexColorComponent);
 
-    return (r_component << 16) | (g_component << 8) | b_component;
+    return (r_component << kRedShift) | (g_component << kGreenShift) | b_component;
 }
 
 Color Color::operator+(const Color& other) const {
-    return Color(Red(std::clamp(r_.value + other.r_.value, 0.0, 1.0)),
-                 Green(std::clamp(g_.value + other.g_.value, 0.0, 1.0)),
-                 Blue(std::clamp(b_.value + other.b_.value, 0.0, 1.0)));
+    return Color(std::clamp(r_ + other.r_, 0.0, 1.0), std::clamp(g_ + other.g_, 0.0, 1.0),
+                 std::clamp(b_ + other.b_, 0.0, 1.0));
 }
 
 Color Color::operator*(const Color& other) const {
-    return Color(Red(std::clamp(r_.value * other.r_.value, 0.0, 1.0)),
-                 Green(std::clamp(g_.value * other.g_.value, 0.0, 1.0)),
-                 Blue(std::clamp(b_.value * other.b_.value, 0.0, 1.0)));
+    return Color(std::clamp(r_ * other.r_, 0.0, 1.0), std::clamp(g_ * other.g_, 0.0, 1.0),
+                 std::clamp(b_ * other.b_, 0.0, 1.0));
 }
 
 Color Color::operator*(double scalar) const {
-    return Color(Red(std::clamp(r_.value * scalar, 0.0, 1.0)),
-                 Green(std::clamp(g_.value * scalar, 0.0, 1.0)),
-                 Blue(std::clamp(b_.value * scalar, 0.0, 1.0)));
+    return Color(std::clamp(r_ * scalar, 0.0, 1.0), std::clamp(g_ * scalar, 0.0, 1.0),
+                 std::clamp(b_ * scalar, 0.0, 1.0));
 }
 
 Color& Color::operator+=(const Color& other) {
-    r_.value = std::clamp(r_.value + other.r_.value, 0.0, 1.0);
-    g_.value = std::clamp(g_.value + other.g_.value, 0.0, 1.0);
-    b_.value = std::clamp(b_.value + other.b_.value, 0.0, 1.0);
+    r_ = std::clamp(r_ + other.r_, 0.0, 1.0);
+    g_ = std::clamp(g_ + other.g_, 0.0, 1.0);
+    b_ = std::clamp(b_ + other.b_, 0.0, 1.0);
     return *this;
 }
 
 Color& Color::operator*=(const Color& other) {
-    r_.value = std::clamp(r_.value * other.r_.value, 0.0, 1.0);
-    g_.value = std::clamp(g_.value * other.g_.value, 0.0, 1.0);
-    b_.value = std::clamp(b_.value * other.b_.value, 0.0, 1.0);
+    r_ = std::clamp(r_ * other.r_, 0.0, 1.0);
+    g_ = std::clamp(g_ * other.g_, 0.0, 1.0);
+    b_ = std::clamp(b_ * other.b_, 0.0, 1.0);
     return *this;
 }
 
 Color& Color::operator*=(double scalar) {
-    r_.value = std::clamp(r_.value * scalar, 0.0, 1.0);
-    g_.value = std::clamp(g_.value * scalar, 0.0, 1.0);
-    b_.value = std::clamp(b_.value * scalar, 0.0, 1.0);
+    r_ = std::clamp(r_ * scalar, 0.0, 1.0);
+    g_ = std::clamp(g_ * scalar, 0.0, 1.0);
+    b_ = std::clamp(b_ * scalar, 0.0, 1.0);
     return *this;
 }
 
--- src/utils/color.h
+++ src/utils/color.h
@@ -1,32 +1,10 @@
 #pragma once
 
-#include <algorithm>
 #include <cstdint>
 
 namespace renderer {
-struct Red {
-    double value;
-    explicit constexpr Red(double value) : value(std::clamp(value, 0.0, 1.0)) {
-    }
-};
-
-struct Green {
-    double value;
-    explicit constexpr Green(double value) : value(std::clamp(value, 0.0, 1.0)) {
-    }
-};
-
-struct Blue {
-    double value;
-    explicit constexpr Blue(double value) : value(std::clamp(value, 0.0, 1.0)) {
-    }
-};
-
 class Color {
 public:
-    explicit constexpr Color(Red r = Red{0.0}, Green g = Green{0.0}, Blue b = Blue{0.0})
-        : r_(r), g_(g), b_(b) {
-    }
     Color(uint32_t hex);
     uint32_t GetHex() const;
     Color operator+(const Color& other) const;
@@ -37,14 +15,16 @@ public:
     Color& operator*=(double scalar);
 
 private:
-    Red r_;
-    Green g_;
-    Blue b_;
+    double r_;
+    double g_;
+    double b_;
+
+    Color(double r, double g, double b);
 };
 
 Color operator*(double scalar, const Color& color);
 
-static constexpr Color kBlackColor = Color{Red{0.0}, Green{0.0}, Blue{0.0}};
-static constexpr Color kWhiteColor = Color{Red{1.0}, Green{1.0}, Blue{1.0}};
-static constexpr Color kDefaultAmbient = Color{Red{0.1}, Green{0.1}, Blue{0.1}};
+static const Color kBlackColor = Color(0x000000);
+static const Color kWhiteColor = Color{0xffffff};
+static const Color kDefaultAmbient = Color{0x1a1a1a};
 }  // namespace renderer

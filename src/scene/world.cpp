029dd6c0ed78999fa228a42a6deec5a8e773e210
Kirill Agafonov <kaagafonov@edu.hse.ru>
2025-03-29 17:38:38 +0300

    Fix World class

--- src/scene/world.cpp
+++ src/scene/world.cpp
@@ -3,8 +3,7 @@
 namespace renderer {
 World::World(std::vector<Object> objects) : objects_(objects) {
     lights_.push_back(Light::Ambient(kDefaultAmbient, 1.0));
-    lights_.push_back(
-        Light::Directional(Vector3(-1.0, -1.0, -1.0), Color{Red{1.0}, Green{0.9}, Blue{0.7}}, 0.8));
+    lights_.push_back(Light::Directional(Vector3(-1.0, -1.0, -1.0), kDefaultDirectional, 0.8));
 }
 
 const std::vector<Object> &World::GetObjects() const {
--- src/utils/color.h
+++ src/utils/color.h
@@ -27,4 +27,5 @@ Color operator*(double scalar, const Color& color);
 static const Color kBlackColor = Color(0x000000);
 static const Color kWhiteColor = Color{0xffffff};
 static const Color kDefaultAmbient = Color{0x1a1a1a};
+static const Color kDefaultDirectional = Color{0xffe6b2};
 }  // namespace renderer
